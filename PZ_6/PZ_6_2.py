ccaea2860a7900e328549b51273f7b8b865d9aab
Veronika Anufrieva <83185711+ReMueff@users.noreply.github.com>
2021-11-27 16:07:28 +0300

    Update

--- PZ_6/PZ_6_2.py
+++ PZ_6/PZ_6_2.py
@@ -1,3 +1,52 @@
 # Дан список размера N. Найти номера тех элементов списка, которые больше своего
 # правого соседа, и количество таких элементов. Найденные номера выводить в порядке
 # их возрастания.
+from random import randint
+
+
+# def row(n):
+#     counter_list = []
+#     counter = []
+#     my_list = [randint(1, 100) for i in range(n)]
+#     dictionary = {}
+#     print(my_list)
+#     for i in range(1, n):
+#         # my_list.append(randint(1, 100))
+#         if my_list[i - 1] > my_list[i]:
+#             print(i - 1)
+#     # print(my_list)
+#     # for i in my_list:
+#     #     if i > i+1:
+#     #         print(i)
+#     #         # dictionary.update({'my_list[i]' : i})
+#     #     # else:
+#     #     #     continue
+#     # #  dictionary = dict(zip(counter_list, counter))
+#     # return dictionary
+#
+#
+# row(int(input(f"Введите предел числового ряда")))
+
+
+def row(n):
+    my_list = []
+    inn = 1
+    counter = []
+    counter_list = []
+    count = 0
+    for i in range(n):
+        my_list.append(randint(1, 100))
+    print(my_list)
+    for i in range(int(len(my_list)) - 1):
+        if my_list[i] > my_list[inn]:
+            counter.append(inn - 1)
+            counter_list.append(my_list[i])
+            count += 1
+        inn += 1
+    dictionary = dict(zip(counter, counter_list))
+    return dictionary, count
+
+final_dict, final_count = (row(int(input(f'Введите предел числового ряда:  '))))
+
+print(f'\nКоличество: {final_count}. \n\nПоследовательность: {final_dict}')
+
--- PZ_6/PZ_6_3.py
+++ PZ_6/PZ_6_3.py
@@ -1,3 +1,22 @@
 # Дан список A размера N и целые числа K и L (1 < K< L < N). Переставить в обратном
 # порядке элементы списка, расположенные между элементами AK и AL, не включая эти
 # элементы
+from random import randint
+
+
+def row(n, k, l):
+    my_list = []
+    for i in range(n):
+        my_list.append(randint(1, 100))
+    print(my_list)
+    res_list = my_list[k:l-1]
+    print(res_list)
+    res_list.reverse()
+    print(res_list)
+    my_list[k:l-1] = res_list
+    print(my_list)
+
+end = int(input('Предел числового ряда:   '))
+first_el = int(input('Первая граница:   '))
+last_el = int(input('Вторая граница:   '))
+row(end, first_el, last_el)
