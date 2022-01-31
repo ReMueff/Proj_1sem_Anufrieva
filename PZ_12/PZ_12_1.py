c9600323e94884b5f8b8f7d922f9a6fc54d8a9c7
Sandr-py <veronika.anuf39@gmail.com>
2022-02-01 02:11:21 +0300

    Update PZ_12

--- PZ_12/PZ_12_1.py
+++ PZ_12/PZ_12_1.py
@@ -1,10 +1,13 @@
+# Реализовать прототип в IDE PyCharm Community с применением пакета tk.
+# Получить интерфейс максимально приближенный к оригиналу.
+
 from tkinter import *
 from tkinter import ttk
 
 My_list = ['First Name', 'Last Name', 'Email Address', 'User Name', 'Password', 'Verify Password']
 y = 180
 
-root = Tk()
+root = Tk()  # Инициализируем корневое окно приложения.
 root.title("Register")
 root.geometry('500x700')
 
@@ -15,9 +18,12 @@ Label(bg='#20242d', text='Register', fg='black', font='comforta 20').place(x=10,
 Label(bg='#20242d').place(x=10, y=101,
                           width=480, height=570)
 
-for i in My_list:
+for i in My_list:  # Создание подписей полей ввода.
     Label(text=i, bg='#20242d', fg='white', font=('comforta', 10)).place(x=50, y=y)
     y += 70
+
+# Добавление панелей ввода.
+
 
 Entry(bg='#13161b', fg='#444444', font=('comforta', 15)).place(x=50, y=130,
                                                                width=400, height=50)
@@ -31,24 +37,19 @@ Entry(bg='#13161b', fg='#444444', font=('comforta', 15)).place(x=50, y=410,
                                                                width=400, height=50)
 Entry(bg='#13161b', fg='#444444', font=('comforta', 15)).place(x=50, y=480,
                                                                width=400, height=50)
+# Добавление поля соглашения (в процессе работы с данным виджетом не оказалось возможным заменить фон)
 
 Var1 = IntVar()
 chk = ttk.Checkbutton(variable=Var1, onvalue=1, offvalue=0)
 chk.place(x=50, y=570, anchor=CENTER)
 
-# x=50, y=550, width=20, height=20
-
-
-# chk = BooleanVar()
-# chk.set(False)
-# ok = Checkbutton(root, text="", var=chk).place(x=50, y=550, width=20, height=20)
-
-
 label1 = Label(root, text="I agree to the Terms and Conditions", bg='#20242d', fg='#444444',
                font=('Arial Bold', 12)).place(x=60, y=555, width=300, height=30)
+
+# Добавление кнопки отправки данных.
 
 button = Button(root, text='Sign me UP-->', bg='#8FBC8F', fg='#006400', font=('Arial Bold', 13))
 button.place(x=90, y=610, width=300, height=50)
 
-root.resizable(width=False, height=False)
+root.resizable(width=False, height=False)   # Блокировка масштабирования окна.
 root.mainloop()
--- PZ_12/PZ_12_2.py
+++ PZ_12/PZ_12_2.py
@@ -4,13 +4,16 @@
 # Создать приложение к данной задаче в Tkinter.
 
 
-
 from tkinter import *
 from tkinter import ttk
 
 
+# Функция-обработчик нажатия кнопки: вывод будет происходить путём изменения скрытого текстового поля.
+
 def square_count():
     a = side_a.get()
+    # Обработка исключений
+    # (невозможно остановить выполнение функции сразу при вводе неверных параметров, решение будет показано далее)
     try:
         a = float(a)
     except ValueError:
@@ -27,6 +30,8 @@ def square_count():
         c = float(c)
     except ValueError:
         Fin_label.configure(text='Сторона квадрата введена неверно!', fg='red')
+
+    # В случае невозможности присваивания float типа данным, происходит повторная проверка данных и обработка ошибок).
 
     if type(a) != float or type(b) != float or type(c) != float:
         Fin_label.configure(text='Сторона квадрата введена неверно!', fg='red')
@@ -45,34 +50,31 @@ def square_count():
         Fin_label.configure(text=f'Количество квадратов: {i}')
 
 
+# Инициализация корневого окна приложения.
 
 root = Tk()
 root.title('Счётчик квадратов')
 root.geometry('450x500')
 
-
 main = Label(text='Счётчик квадратов', font=('Arial', 20)).grid(columnspan=2, row=0)
 
-side_a = Entry(master=main, width=10)
+side_a = Entry(master=main, width=10)   # Добавление полей для ввода информации.
 side_a.grid(row=1, column=0, pady=5, padx=5)
-lab_sidea = Label(text='Введите первую сторону внешней прямоугольной области', font=('Arial', 10)).grid(row=1, column=1, pady=5)
+lab_sidea = Label(text='Введите первую сторону внешней прямоугольной области', font=('Arial', 10)).grid(row=1, column=1,
+                                                                                                        pady=5)
 side_b = Entry(master=main, width=10)
 side_b.grid(row=2, column=0, padx=5)
-lab_sideb = Label(text='Введите вторую сторону внешней прямоугольной области', font=('Arial', 10)).grid(row=2, column=1, pady=5)
+lab_sideb = Label(text='Введите вторую сторону внешней прямоугольной области', font=('Arial', 10)).grid(row=2, column=1,
+                                                                                                        pady=5)
 val_c = Entry(master=main, width=10)
 val_c.grid(row=3, column=0, padx=5)
-lab_valc = Label(text='Введите сторону внутренних квадратов', font=('Arial', 10)).grid(row=3, column=1, pady=5, sticky=W)
-
+lab_valc = Label(text='Введите сторону внутренних квадратов', font=('Arial', 10)).grid(row=3, column=1, pady=5,
+                                                                                       sticky=W)
+# Добавление кнопки, к которой привязан обработчик.
 button = Button(text='Accept', font=('Arial', 12), command=square_count)
-button.grid(row=4, columnspan=2, sticky=W+E, padx=5)
+button.grid(row=4, columnspan=2, sticky=W + E, padx=5)
 
-Fin_label = Label(text='', font=('Arial', 12))
+Fin_label = Label(text='', font=('Arial', 12))   # Добавление изменяемого текстового поля.
 Fin_label.grid(row=5, columnspan=2)
-
-
-
-
-
-
 
 root.mainloop()
