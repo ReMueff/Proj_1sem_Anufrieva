473976c8916a1ced103d24fe06667d1d3da988a6
ReMueff <veronika.anuf39@gmail.com>
2022-04-13 18:01:43 +0300

    Update

--- DataBaseProject/main.py
+++ DataBaseProject/main.py
@@ -8,5 +8,12 @@ with connect('saper.db') as con:
 #     cur.executemany("INSERT INTO NewTable VALUES (?, ?, ?, ?, ?)", table)
     print(cur.execute("SELECT * FROM NewTable").fetchall())
     print(cur.execute("SELECT * FROM NewTable WHERE sex=2").fetchall())
+    print(cur.execute("SELECT * FROM NewTable WHERE score<1000").fetchall())
+    print(cur.execute("SELECT * FROM NewTable WHERE old BETWEEN 18 AND 20").fetchall())
+    cur.execute("UPDATE NewTable SET old=19 WHERE old=19")
+    cur.execute("UPDATE NewTable SET score=score+300 WHERE score<1000")
+    cur.execute("UPDATE NewTable SET score=score+100 WHERE old=20")
+    cur.execute("DELETE FROM NewTable WHERE score<1000")
+
 
 
